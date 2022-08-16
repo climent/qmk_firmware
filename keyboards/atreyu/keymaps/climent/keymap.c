afeaae0cd6fd0ea123b279f5c621f0a5d2a0c5e4
Jesus Climent <climent@gmail.com>
2022-08-16 07:18:25 +0100

    Atreyu local changes.

--- keyboards/atreyu/keymaps/climent/keymap.c
+++ keyboards/atreyu/keymaps/climent/keymap.c
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-<<<<<<< HEAD
 #include "atreyu.h"
 #include "eeconfig.h"
 
@@ -23,24 +22,11 @@
 #define _ADJUST 3
 #define _NAV 4
 #define _SYM 5
-=======
-
-#include QMK_KEYBOARD_H
-
-
-enum custom_layers {
-    _QWERTY,
-    _LOWER,
-    _RAISE,
-    _ADJUST
-};
->>>>>>> upstream/master
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LWR,
   RSE,
-<<<<<<< HEAD
   ADJ,
   NAV,
   SYM
@@ -59,16 +45,10 @@ enum custom_keycodes {
 #define KC_LCTL_T LCTL_T
 #define KC_GUI_T GUI_T
 #define KC_ALT_T ALT_T
-=======
-  ADJ
-};
-
->>>>>>> upstream/master
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
-<<<<<<< HEAD
      KC_ESC  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                KC_6   , KC_7   , KC_8   , KC_9   , KC_0    ,KC_GRV ,
      KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                KC_Y   , KC_U   , KC_I   , KC_O   , KC_P    ,KC_BSPC ,
      KC_LCTRL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN ,LT(_SYM,KC_QUOT) ,
@@ -115,43 +95,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTRL,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_LT,   KC_LPRN, KC_RPRN, KC_GT,   XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, _______,
                    XXXXXXX, KC_LALT, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-=======
-     KC_ESC  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0    ,KC_GRV  ,
-     KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P    ,KC_BSPC ,
-     KC_LCTRL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN ,KC_QUOT ,
-     KC_LSFT , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH ,KC_RSFT ,
-     KC_LCTL,           LWR    , KC_LALT, KC_LGUI, KC_ENT , KC_LBRC, KC_RBRC, KC_SPC , RSE    , KC_NO  ,  KC_NO ,          KC_RCTL 
-                
-  ),
-
-[_RAISE] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                    KC_HOME, KC_END , _______, KC_LCBR, KC_RCBR, KC_TILD,
-  _______, _______, _______, _______, KC_HOME, KC_END ,                    KC_PGUP, XXXXXXX, KC_UP  , KC_LBRC, KC_RBRC, KC_DEL ,
-  _______, _______, _______, _______, _______, _______,                    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_PIPE, KC_ENT ,
-  _______, _______, _______, _______, _______, _______,                    KC_UNDS, KC_MINS, KC_EQL , KC_PLUS, KC_BSLS, _______,
-  _______,          _______, _______, _______, KC_SPC , KC_HOME, KC_END  , _______, _______, KC_RGUI, KC_RALT,          _______
-),
-
-[_LOWER] = LAYOUT(
-  KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
-  _______, KC_VOLU, _______, _______, TG(_RAISE), AG_TOGG,                _______, _______, _______, _______, _______, KC_DEL ,
-  _______, KC_VOLD, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, 
-  _______, KC_CAPS, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-  _______,          _______, _______, _______, KC_SPC , KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______
-),
-
-[_ADJUST] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______,          _______, _______, _______, _______ , _______, _______ , _______, _______, _______, _______,         _______
->>>>>>> upstream/master
 )
 
 };
 
-<<<<<<< HEAD
 #ifdef COMBO_ENABLE
 
 enum combo_events {
@@ -203,10 +150,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
 
-=======
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
->>>>>>> upstream/master
     case LWR:
       if (record->event.pressed) {
         layer_on(_LOWER);
@@ -222,7 +165,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         layer_on(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-<<<<<<< HEAD
         update_tri_layer(_NAV, _RAISE, _NAV);
       } else {
         layer_off(_RAISE);
@@ -246,11 +188,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_ADJUST);
       } else {
         layer_off(_ADJUST);
-=======
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
->>>>>>> upstream/master
       }
       return false;
       break;
@@ -266,7 +203,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return true;
       break;
-<<<<<<< HEAD
 
     case KC_TAB:
       {
@@ -322,9 +258,3 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 }
 
 #endif
-=======
-  }
-  return true;
-}
-
->>>>>>> upstream/master
