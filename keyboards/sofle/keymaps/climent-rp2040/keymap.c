36306111b54adc7db4a3b01885790d79fc79f21e
Jesus Climent <climent@gmail.com>
2022-11-16 10:13:22 +0000

    Updated media keys for LOWER layer.

--- keyboards/sofle/keymaps/climent-rp2040/keymap.c
+++ keyboards/sofle/keymaps/climent-rp2040/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT( \
   KC_GRV ,    KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                         KC_F6  , KC_F7  ,      KC_F8,   KC_F9,      KC_F10 , _______,
-  _______,    KC_F11 , KC_F12 , _______, TG(_RAISE), AG_TOGG,                      _______, KC_VOLD,      KC_MUTE, KC_VOLU,    _______, KC_DEL ,
+  _______,    KC_F11 , KC_F12 , _______, TG(_RAISE), AG_TOGG,                      _______, KC_MPRV,      KC_MPLY, KC_MNXT,    _______, KC_DEL ,
   G(C(KC_Q)), _______, _______, _______, DF(_QWERTY), DF(_COLEMAK),                _______, C(KC_LEFT),   _______, C(KC_RGHT), _______, _______,
   _______,    KC_CAPS, _______, _______, C(KC_LEFT), C(KC_RGHT), _______, _______, _______, C(S(KC_TAB)), G(KC_R), C(KC_TAB),  _______, _______,
                     _______, _______, _______, _______, KC_SPC ,          _______, _______, _______, _______, _______ \
