59047e47dd554bf993d71c428e59b701fc9c8230
Jesus Climent <climent@gmail.com>
2023-02-28 09:13:57 +0000

    Updated keymap on the Soufflé

--- keyboards/sofle/keymaps/climent-rp2040/keymap.c
+++ keyboards/sofle/keymaps/climent-rp2040/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
      KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(_SYM,KC_QUOT),
      KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE, C(KC_LALT),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-	 KC_NAV, KC_LOWER, KC_LALT ,KC_LGUI, KC_ENT,                           KC_SPC,KC_RAISE,KC_NAV, KC_NUM, KC_RCTL
+	 KC_NAV, KC_LOWER, KC_LALT ,KC_LGUI, KC_ENT,                           KC_SPC,KC_RAISE,KC_NAV, KC_BSPC, KC_RCTL
 ),
 
 [_COLEMAK] = LAYOUT( \
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                       KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
      KC_LCTL,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                       KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    LT(_SYM,KC_QUOT),
      KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE, C(KC_LALT),  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-	 KC_NAV, KC_LOWER, KC_LALT ,KC_LGUI, KC_ENT,                           KC_SPC,KC_RAISE,KC_NAV, KC_NUM, KC_RCTL
+	 KC_NAV, KC_LOWER, KC_LALT ,KC_LGUI, KC_ENT,                           KC_SPC,KC_RAISE,KC_NAV, KC_BSPC, KC_RCTL
 ),
 [_RAISE] = LAYOUT(\
   _______, _______, _______, _______, _______, _______,                    KC_HOME, KC_END , _______, KC_LCBR, KC_RCBR, KC_TILD, \
