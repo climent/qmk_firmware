86d6415b7ea64450778882e89e013f001fb807ba
Jesus Climent <climent@gmail.com>
2025-12-18 17:54:10 +0100

    Update rows to match the latest handwired board, codenamed bonchan.

--- compile.sh
+++ compile.sh
@@ -2,3 +2,5 @@ qmk compile --keyboard sofle --keymap climent
 qmk compile --keyboard sofle --keymap climent-rp2040
 qmk compile --keyboard atreyu --keymap pcb
 qmk compile --keyboard atreyu --keymap pcb-rp2040
+qmk compile -kb atreyu/handwired -km default
+qmk compile -kb atreyu/handwired -km default -e CONVERT_TO=liatris
