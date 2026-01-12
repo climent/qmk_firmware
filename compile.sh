0fdc3a9484a61baf40523f35f0a571599e5b9878
Jesus Climent <climent@gmail.com>
2026-01-12 19:10:08 +0100

    Updated handwired via keyboard for Atreyu.

--- compile.sh
+++ compile.sh
@@ -4,3 +4,4 @@ qmk compile --keyboard atreyu --keymap pcb
 qmk compile --keyboard atreyu --keymap pcb-rp2040
 qmk compile -kb atreyu/handwired -km default
 qmk compile -kb atreyu/handwired -km default -e CONVERT_TO=liatris
+qmk compile -kb atreyu/handwired -km via -e CONVERT_TO=liatris
